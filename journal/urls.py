a6317b9f643159e8c884f405ebd4e3ddf91a5b77
t <1123013@wfsh.tp.edu.tw>
2024-07-05 22:31:27 +0800

    新增日誌

--- journal/urls.py
+++ journal/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('' , JournalList.as_view() , name = 'jou_list')
+    path('' , JournalList.as_view() , name = 'jou_list'),
+    path('create/' , JournalCreate.as_view() , name = 'jou_create'),
 ]
--- journal/views.py
+++ journal/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView , CreateView
 from .models import Journal
+from django.urls import reverse_lazy
 # Create your views here.
 class JournalList(ListView):
     model = Journal
     ordering = ['-id']
-    
+
+class JournalCreate(CreateView):
+    model = Journal
+    fields = ['content']
+    success_url = reverse_lazy('jou_list')
+    template_name = 'form.html'
