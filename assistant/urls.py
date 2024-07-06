2e23b49b22af2e69abd684415827d7720beb508a
t <1123013@wfsh.tp.edu.tw>
2024-07-06 15:42:00 +0800

    分頁連結

--- assistant/urls.py
+++ assistant/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('journal/' , include('journal.urls')),
+    path('' ,RedirectView.as_view(url = 'journal/')),
     path('accounts/' , include('django.contrib.auth.urls'))
 ]
--- journal/views.py
+++ journal/views.py
@@ -2,28 +2,31 @@ from django.shortcuts import render
 from django.views.generic import ListView , CreateView , UpdateView , DeleteView
 from .models import Journal
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 #日誌列表
-class JournalList(ListView):
+class JournalList(LoginRequiredMixin , ListView):
     model = Journal
     ordering = ['-id']
+    paginate_by = 3    #設定每頁最多顯示資料筆數
 
 #日誌新增
-class JournalCreate(CreateView):
+class JournalCreate(LoginRequiredMixin , CreateView):
     model = Journal
     fields = ['content']
     success_url = reverse_lazy('jou_list')
     template_name = 'form.html'
 
 #日誌修改
-class JournalEdit(UpdateView):
+class JournalEdit(LoginRequiredMixin , UpdateView):
     model = Journal
     fields = ['content']
     success_url = reverse_lazy('jou_list')
     template_name = 'form.html'
 
 #日誌刪除
-class JournalDelete(DeleteView):
+class JournalDelete(LoginRequiredMixin , DeleteView):
     model = Journal
     success_url = reverse_lazy('jou_list')
     template_name = 'confirm_delete.html'
