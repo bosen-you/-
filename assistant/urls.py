fe61b6b1816d6d8c6398cc274ecbd58297c4ac3a
t <1123013@wfsh.tp.edu.tw>
2024-07-06 14:14:07 +0800

    login , logged out

--- assistant/urls.py
+++ assistant/urls.py
@@ -19,5 +19,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('journal/' , include('journal.urls'))
+    path('journal/' , include('journal.urls')),
+    path('accounts/' , include('django.contrib.auth.urls'))
 ]
