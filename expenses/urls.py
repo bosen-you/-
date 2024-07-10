9fb6295536d2dcb808e326200b6985ac656d3643
t <1123013@wfsh.tp.edu.tw>
2024-07-10 11:13:12 +0800

    新增，修改，刪除支出紀錄

--- expenses/urls.py
+++ expenses/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('' , ExpenseList.as_view() , name = 'exp_list')
+    path('' , ExpenseList.as_view() , name = 'exp_list'),
+    path('create' , ExpenseCreate.as_view() , name = 'exp_create'),
+    path('<int:pk>/update/' , ExpenseUpdate.as_view() , name = 'exp_update'),
+    path('<int:pk>/delete/' , ExpenseDelete.as_view() , name = 'exp_delete'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import *
 from .models import Expense
+from django.urls import reverse_lazy
 
 # Create your views here.
 #日誌列表紀錄
@@ -7,3 +8,25 @@ class ExpenseList(ListView):
     model = Expense
     ordering = ['-id']
     paginate_by = 10
+
+#新增支出紀錄
+class ExpenseCreate(CreateView):
+    model = Expense
+    fields = '__all__'
+    template_name = 'form.html'
+    success_url = reverse_lazy('exp_list')
+
+#修改支出紀錄
+class ExpenseUpdate(UpdateView):
+    model = Expense
+    fields = '__all__'
+    template_name = 'form.html'
+    success_url = reverse_lazy('exp_list')
+
+#刪除支出紀錄
+class ExpenseDelete(DeleteView):
+    model = Expense
+    template_name = 'confirm_delete.html'
+    success_url = reverse_lazy('exp_list')
+
+    
